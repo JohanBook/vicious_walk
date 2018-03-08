afe1bf03454e82b0450b744c242cdc5f254e6989
Johan Book <johanbook@hotmail.com>
2018-03-08 18:01:25 +0100

    Update GUI.java

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,4 +1,10 @@
-// A class for GUI. Do not edit anything in here.
+////////////////////////////////////////////////
+// GUI.java
+// A class for GUI. Do not edit.
+// Johan Book
+// 2015-09-12
+////////////////////////////////////////////////
+
 package gui;
 
 import java.awt.BorderLayout;
@@ -18,13 +24,15 @@ import javax.swing.Timer;
 import character.Character;
 
 @SuppressWarnings("serial")
-public class GUI extends JFrame {
+public class GUI extends JFrame 
+{
 	private BufferedImage image;
 	private List<Character> Characters;
 	private final int CHARACTER_SIZE = 5;
 
 	// Constructor
-	public GUI(List<Character> Characters, java.awt.Dimension dimension) {
+	public GUI(List<Character> Characters, java.awt.Dimension dimension) 
+	{
 		this.Characters = Characters;
 
 		// Menu
@@ -33,16 +41,17 @@ public class GUI extends JFrame {
 
 		btnSave.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e) 
+			{
 				saveImage("image");
 			}
 		});
-
 		menu.add(btnSave);
 
 		Timer timer = new Timer(50, new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e) 
+			{
 				repaint();
 			}
 		});
@@ -56,54 +65,54 @@ public class GUI extends JFrame {
 		add(panel, BorderLayout.CENTER);
 		add(menu, BorderLayout.SOUTH);
 
-		setTitle("The VicWak project");
+		setTitle("A Vicious Walk");
 		pack();
 		setVisible(true);
 		requestFocus();
 	}
 
 	// ================ Panel ================
-	private class Panel extends JPanel {
+	private class Panel extends JPanel 
+	{
 		@Override
-		protected void paintComponent(Graphics gr) {
+		protected void paintComponent(Graphics gr) 
+		{
 			super.paintComponent(gr);
 			try {
 				image = drawToImage(this.getWidth(), this.getHeight());
 			} catch (Exception e) {
-
+				System.out.println("ERROR: Unable to draw image to panel");
 			}
 			gr.drawImage(image, 0, 0, null);
 		}
 	}
 
 	// Method for drawing graphics and storing it in an image
-	private BufferedImage drawToImage(int X, int Y) {
-		BufferedImage image = new BufferedImage(X, Y,
-				BufferedImage.TYPE_INT_ARGB);
+	private BufferedImage drawToImage(int X, int Y) 
+	{
+		BufferedImage image = new BufferedImage(X, Y,BufferedImage.TYPE_INT_ARGB);
 		Graphics gr = image.createGraphics();
 
-		for (Character Character : Characters) {
+		for (Character Character : Characters) 
+		{
 			gr.setColor(Character.getColor());
-			gr.fillOval((int) Character.getPosition()[0],
-					(int) Character.getPosition()[1], CHARACTER_SIZE,
-					CHARACTER_SIZE);
+			gr.fillOval((int) Character.getPosition()[0],(int) Character.getPosition()[1], CHARACTER_SIZE,CHARACTER_SIZE);
 			gr.setColor(Color.BLACK);
-			gr.drawOval((int) Character.getPosition()[0],
-					(int) Character.getPosition()[1], CHARACTER_SIZE,
-					CHARACTER_SIZE);
+			gr.drawOval((int) Character.getPosition()[0],(int) Character.getPosition()[1], CHARACTER_SIZE,CHARACTER_SIZE);
 		}
 
 		return image;
 	}
 
-	private void saveImage(String string) {
-		try {
+	private void saveImage(String string) 
+	{
+		try 
+		{
 			File outputfile = new File("data/" + string + ".png");
 			javax.imageio.ImageIO.write(image, "png", outputfile);
 			System.out.println("Image saved");
 		} catch (Exception e) {
 			System.out.println("ERROR: Unable to save image to file");
 		}
-
 	}
 }
